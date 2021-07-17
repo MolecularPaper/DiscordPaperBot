2ceb7bf206502da19599ecf0c8b65403f94fd19f
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2021-07-18 04:27:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=None)
-    print("봇 이름",bot.user.name,"봇아이디: ", bot.user.id, "봇 버전: ", discord.__version__)
+    print("봇 이름", bot.user.name, "봇아이디: ", bot.user.id, "봇 버전: ", discord.__version__)
 
-bot.run('os.environ['token'])
+
+bot.run(os.environ['token'])
