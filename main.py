a673728a50d3951e21097f00ef47323dc07f7571
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2021-07-18 04:50:25 +0900

    Command Add

--- main.py
+++ main.py
@@ -11,4 +11,19 @@ async def on_ready():
     print("봇 이름", bot.user.name, "봇아이디: ", bot.user.id, "봇 버전: ", discord.__version__)
 
 
+@bot.event()
+async def on_message(message):
+    if message.author.bot:
+        return None
+
+    if message.content.startwith('!안녕'):
+        channel = message.channel
+        await  channel.send('ㅎㅇ')
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('ㅎㅇ')
+
+
 bot.run(os.environ['token'])
