46875cf21cf8dd09a218224877544b2b85bc5b74
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2021-07-18 11:10:20 +0900

    Edit

---
+++ chat.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+
+class Chat(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='페이퍼')
+    async def call_paper_kr(ctx, *args):
+        str = ' '.join(args)
+        if str == "ㅎㅇ":
+            await ctx.send('ㅎㅇ')
+        if str == "병신":
+            await ctx.send('ㄹㅇ ㅋㅋ')
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import discord
+import discord, os, music, chat
 from discord.ext import commands
-
-import os
-
 bot = commands.Bot(command_prefix='!')
-
 
 @bot.event
 async def on_ready():
@@ -12,44 +8,5 @@ async def on_ready():
     print("봇 이름", bot.user.name, "봇아이디: ", bot.user.id, "봇 버전: ", discord.__version__)
     print("페이퍼봇 준비 완료!")
 
-
-@bot.event
-async def on_message(message):
-    if message.content == "안녕!":
-        await message.channel.send("안녕!")
-
-    await bot.process_commands(message)
-
-
-@bot.command(name='안녕')
-async def hello(ctx):
-    await ctx.send('ㅎㅇ')
-
-
-@bot.command(name='join')
-async def voice_join(ctx):
-    if ctx.author.voice:
-        channel = ctx.message.author.voice.channel
-        await channel.connect()
-    else:
-        await ctx.send("음성채널에 들어가있어야 됩니다!")
-
-
-@bot.command(name='leave')
-async def voice_leave(ctx):
-    if ctx.voice_client:
-        await ctx.guild.voice_client.disconnect()
-    else:
-        await ctx.send("저 음성채널에 안들어가있는데요?")
-
-
-@bot.command(name='페이퍼')
-async def call_paper(ctx, *args):
-    str = ' '.join(args)
-    if str == "ㅎㅇ":
-        await ctx.send('ㅎㅇ')
-    if str == "병신":
-        await ctx.send('ㄹㅇ ㅋㅋ')
-
-
+bot.add_cog(chat.Chat(bot))
 bot.run(os.environ['token'])
