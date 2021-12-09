50bb605cc7518ef5b286b32d0baa440d24f88d11
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2021-12-09 10:05:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ async def on_ready():
     print("봇 이름", bot.user.name, "봇아이디: ", bot.user.id, "봇 버전: ", discord.__version__)
     print("페이퍼봇 준비 완료!")
 
-bot.add_cog(commandInfo.CommandInfo(bot))
-bot.add_cog(info.Info(bot))
-bot.add_cog(chat.Chat(bot))
-bot.run(os.environ['token'])
-
 
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send('그런 명령어는 없는데요...?')
+
+
+bot.add_cog(commandInfo.CommandInfo(bot))
+bot.add_cog(info.Info(bot))
+bot.add_cog(chat.Chat(bot))
+bot.run(os.environ['token'])
