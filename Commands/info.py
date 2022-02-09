1800cdf7e147bc86f74fae9e5d3b90617734ebba
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2022-02-09 14:17:57 +0900

    Update

--- Commands/info.py
+++ Commands/info.py
@@ -2,17 +2,10 @@ import discord
 from discord.ext import commands
 
 
-async def execution(ctx):
-    _description = '이 봇은 MolecularPaper가 제작하였습니다.\n\n 제작을 도와준사람(닉네임): 누군가'
-    _embed = discord.Embed(title='페이퍼봇 정보', description=_description, color=0x00ff00)
-    await ctx.send(embed=_embed)
-
-
 class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-<<<<<<< HEAD
     # 봇 정보
     @commands.command(name='정보')
     async def info_bot(self, ctx):
@@ -26,12 +19,3 @@ class Info(commands.Cog):
         _embed = discord.Embed(title='명령어 정보', description='!정보 - 페이퍼봇의 기본적인 정보를 알려줍니다.'
                                                            '\n\n !서버상태 - 디스코드 서버의 상태를 알려줍니다. ', color=0x00ff00)
         await ctx.send(embed=_embed)
-=======
-    @commands.command(name='info')
-    async def paper_info_en(self, ctx):
-        await execution(ctx)
-
-    @commands.command(name='정보')
-    async def paper_info_kr(self, ctx):
-        await execution(ctx)
->>>>>>> 0087976463b3c8a9c91411afd88a65dd195dad5c
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import discord
+import discord, os
 from Commands import info, discord_status
 from Song import song
-=======
-import discord, os
-from Commands import info, chat, commandInfo
->>>>>>> 0087976463b3c8a9c91411afd88a65dd195dad5c
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
@@ -17,21 +12,7 @@ async def on_ready():
     print("봇 이름", bot.user.name, "봇아이디: ", bot.user.id, "봇 버전: ", discord.__version__)
     print("페이퍼봇 준비 완료!")
 
-
-<<<<<<< HEAD
 bot.add_cog(info.Info(bot))
 bot.add_cog(discord_status.Status(bot))
 bot.add_cog(song.Song(bot))
-# bot.run(os.environ['token'])
-bot.run('ODIzNTM2NzA2NDU1NzMyMjM0.YFiQUw.M3mid2CzmKhkTO7HEGJgrGcBjwU')
-=======
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send('그런 명령어는 없는데요...?')
-
-bot.remove_command("help")
-bot.add_cog(commandInfo.CommandInfo(bot))
-bot.add_cog(info.Info(bot))
-bot.add_cog(chat.Chat(bot))
 bot.run(os.environ['token'])
->>>>>>> 0087976463b3c8a9c91411afd88a65dd195dad5c
