1f5cbbb81d6d1755e3130f1e44d39d46c596aaea
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2022-05-19 10:48:44 +0900

    Update

--- Chat/GPT3.py
+++ Chat/GPT3.py
@@ -11,7 +11,7 @@ def request(text):
         return
 
     openai.organization = "org-O1hKvSNjES74EawZRnPeuwZ4"
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     gpt = api.GPT(engine="text-davinci-002", temperature=0.4, max_tokens=100)
     output = gpt.submit_request(text).choices[0]['text'].split(':')
