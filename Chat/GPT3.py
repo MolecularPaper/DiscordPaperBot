cee9229fcc131cf9429437291d9983705483621d
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2022-05-19 11:02:02 +0900

    Update

--- Chat/GPT3.py
+++ Chat/GPT3.py
@@ -10,8 +10,14 @@ def request(text):
     if len(text) > 1500:
         return
 
+    if os.path.isfile('./Data/token.txt'):
+        f = open('./Data/api_key.txt')
+        api_key = f.readline()
+    else:
+        api_key = os.environ['token']
+
     openai.organization = "org-O1hKvSNjES74EawZRnPeuwZ4"
-    openai.api_key = "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"
+    openai.api_key = api_key
 
     gpt = api.GPT(engine="text-davinci-002", temperature=0.4, max_tokens=100)
     output = gpt.submit_request(text).choices[0]['text'].split(':')
