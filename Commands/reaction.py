ccb870defed8078285c97929e06c8dea08302a82
MolecularPaper <43882000+MolecularPaper@users.noreply.github.com>
2022-05-20 13:29:58 +0900

    Update

--- Commands/reaction.py
+++ Commands/reaction.py
@@ -4,28 +4,27 @@ from GPT3 import GPT3
 from discord.ext import commands
 from Commands import utility
 
-reactions = {}
-
-
-# 리액션 읽기
-def read_reaction():
-    f = open('./Data/reaction.txt', 'r', encoding="UTF-8")
-    lines = f.readlines()
-
-    for line in lines:
-        if not line:
-            continue
-        split = line.split(' : ')
-        if reactions.get(split[0]) is None:
-            reactions[split[0]] = [split[1]]
-        else:
-            reactions[split[0]].append(split[1])
-
 class ReAction(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.use_gpt = False
-        read_reaction()
+        self.reactions = self.read_reaction()
+
+    # 리액션 읽기
+    def read_reaction(self):
+        f = open('./Data/reaction.txt', 'r', encoding="UTF-8")
+        lines = f.readlines()
+        reactions = {}
+
+        for line in lines:
+            if not line:
+                continue
+            split = line.split(' : ')
+            if reactions.get(split[0]) is None:
+                reactions[split[0]] = [split[1].replace("\n", "")]
+            else:
+                reactions[split[0]].append(split[1].replace("\n", ""))
+        return reactions
 
     @commands.command(name='배워')
     async def learn(self, ctx, word, *args):
@@ -33,16 +32,18 @@ class ReAction(commands.Cog):
         if ['명령어', '정보', '서버상태', '식사추천', '게임순위'].__contains__(word):
             await ctx.send('어림도 없지!')
             return
-        elif reactions.get(word) is None:
-            reactions[word] = [text]
-        elif text in reactions[word]:
+        if self.reactions[word].__contains__(text):
             await ctx.send('이미 배웠습니다!')
             return
-        elif len(args) == 0:
+        if text.isspace():
             await ctx.send('반응할 문장을 입력해주세요!')
             return
+        
+        if self.reactions.get(word) is None:
+            self.reactions[word] = [text]
         else:
-            reactions[word].append(text)
+            self.reactions[word].append(text)
+            
         await ctx.send(f'{word} - {text}, 확인했습니다.')
         f = open('./Data/reaction.txt', 'a', encoding="UTF-8")
         f.write(f"{word} : {text}\n")
@@ -53,8 +54,8 @@ class ReAction(commands.Cog):
             await utility.send_edit(ctx.channel, '개같은 주인놈...', '**위대하고 뛰어난 저의 창조주십니다!**', 0.3)
         elif msg == '병신':
             await utility.send_edit(ctx.channel, '**지는ㅋ**', '뭐', 0.5)
-        elif msg in reactions:
-            await ctx.channel.send(random.choice(reactions[msg]))
+        elif msg in self.reactions:
+            await ctx.channel.send(random.choice(self.reactions[msg]))
         elif self.use_gpt:
             gpt_out = GPT3.request(msg)
             await ctx.channel.send(gpt_out)
