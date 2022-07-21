5f2b5d50c0dd57e8b7a2141ce004159d04d47386
MolecularPaper <jat05026@gmail.com>
2022-07-21 19:30:20 +0900

    Update

--- Source/mafia_game.py
+++ Source/mafia_game.py
@@ -1,8 +1,11 @@
-import discord, asyncio, random, json
+import discord, asyncio, json, utility
+from multiprocessing.dummy import Array
 from discord.ext import commands
 
 config_path = './Data/Mafia/mafia_game_config.json'
-text_path = './Data/Mafia/mafia_game_text.json'
+text_path = './Data/Mafia/mafia_text.json'
+channel_path = './Data/Mafia/mafia_channel.json'
+trigger_path = './Data/Mafia/mafia_game_trigger.txt'
 
 class MafiaCommand(commands.Cog):
     def __init__(self, bot) -> None:
@@ -15,31 +18,71 @@ class MafiaCommand(commands.Cog):
 class Mafia(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-
-        #게임 기본 옵션 설정 (변경 가능)
-        with open(config_path, 'r', encoding='utf-8') as f:
-            self.config = json.load(f)
+        self.config = self.ReadJson(config_path) #게임 기본 옵션 설정 (변경 가능)
+        self.systext = self.ReadJson(text_path) #게임 텍스트 설정
+        self.channels = self.ReadJson(channel_path) #게임 채널 설정
+        self.triggers = self.ReadJson(trigger_path) #게임 내부 트리거 설정
         
-        #게임 텍스트 설정
-        with open(text_path, 'r', encoding='utf-8') as f:
-            self.game_text = json.load(f)
+        self.CreateChannel()
 
-    async def ExcuteMetod(method_name):
-        await getattr(Mafia, method_name)()
+    async def ReadJson(self, path) -> dict:
+        with open(path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+    async def CreateChannel(self, ctx):
+        for channel in self.channels.keys():
+            self.channels[channel] = await self.create_channel(ctx, '마피아게임', discord.ChannelType.category)
+        return
+    
+    async def Match(self, ctx):
+        await self.SendText(ctx, self.ConvertText(self.systext['match_making']['start']))
+
+    async def MatchStop(self, ctx):
+        await self.SendText(ctx, self.systext['match_making']['stop'])
+        del self
+
+    async def GameStart(self, ctx):
+        #유저수 검사후 시작 가능하면 시작
+        await self.SendText(ctx, self.systext['match_making']['start'])
         return
 
-    async def SendText(self, channel, pointer:str):
-        await channel.send(self.game_text[pointer])
+    async def GameStop(self, ctx):
+        await self.SendText(ctx, self.systext['match_making']['stop'])
+        del self
 
-    async def SendText(self, channel, message:str, change:str, value):
-        await channel.send(message.replace(change, str(value)))
+    async def SendText(self, channel, text:str):
+        await channel.send(text)
 
-    async def WaitTimeSceond(self, pointer:str):
+    async def WaitTimeSceond(self, pointer:str, callback=None):
         await asyncio.sleep(self.config[pointer])
+        if callback is not None:
+            callback()
 
-def ConvertText(text: str, config: json) -> str:
-    return text
+    def ConvertText(self, text: str) -> str:
+        for key, value in self.config.items():
+            text = self.ReplaceText(text, key, value)
+        for key, value in self.channels.items():
+            text = self.ReplaceText(text, key, value)
+        return text
+
+    def ReplaceText(self, text: str, key: str, value):
+        return text.replace('{' + key + '}', str(value))
+
+async def cancel_me():
+     while True:
+        await asyncio.sleep(3600)
+        print("cancel me plz")
+
+def wrapped_coroutine():
+     for task in asyncio.all_tasks():
+         try:
+             task.cancel()
+         except asyncio.CancelledError:
+             print("Task cancelled")
 
 if __name__ == "__main__":
     bot = commands.Bot(command_prefix='테스트')
     mafia = Mafia(bot)
+    
+    cancel_me()
+    wrapped_coroutine()
--- Source/utility.py
+++ Source/utility.py
@@ -1,4 +1,7 @@
 import asyncio
+import discord
+
+from discord.ext import commands
 
 def read_file(filename):
         file = open(f'./Data/{filename}.txt', 'r', encoding="UTF-8").readlines()
@@ -11,3 +14,55 @@ async def send_edit(channel, msg, edit_msg, delay):
     tk = await channel.send(msg)
     await asyncio.sleep(delay)
     await tk.edit(content=edit_msg)
+
+# 채널 생성
+async def create_channel(ctx, bot, name: str, _type: discord.ChannelType, category: discord.CategoryChannel = None, can_read: bool = True, can_send=True, print_log=True):
+    overwrites = {
+        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=can_read, send_messages=can_send),
+        bot.user: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+    }
+
+    if print_log:
+        print(f'[INFO] {category}에 속한 {name} {_type}채널이 [읽기: {can_read}  쓰기: {can_send}] 권한으로 생성됨')
+
+    if _type == discord.ChannelType.text:
+        return await ctx.guild.create_text_channel(name, category=category, overwrites=overwrites)
+    elif _type == discord.ChannelType.voice:
+        return await ctx.guild.create_voice_channel(name, category=category, overwrites=overwrites)
+    elif _type == discord.ChannelType.category:
+        return await ctx.guild.create_category(name)
+    
+
+async def set_channel_permissions(member: discord.Member, channel, can_read: bool = True, can_send: bool = True, print_log=True):
+    '''채널 권한 설정'''
+    perms = channel.overwrites_for(member)
+    perms.read_messages = can_read
+    perms.send_messages = can_send
+    await channel.set_permissions(member, overwrite=perms)
+    
+    if print_log:
+        print(f'[INFO] {channel.name} 채널의 {member}의 권한이 [읽기: {can_read}  쓰기: {can_send}] 으로 변경됨')
+
+async def dm_channel(member: discord.Member):
+    '''DM 채널 반환 (없을경우 생성함)'''
+    channel = member.dm_channel
+    if channel is None:
+        await member.create_dm()
+    return channel
+
+async def send_dm_embed(member: discord.Member, title:str, text: str, color=0x00ff00):
+    '''개인 메세지 보내기 (임베드)'''
+    _embed = discord.Embed(title=title, description=text, color=color)
+    channel = dm_channel(member)
+    await channel.send(embed=_embed)
+
+async def send_dm(member: discord.Member, text: str):
+    '''개인 메세지 보내기'''
+    channel = dm_channel(member)
+    await channel.send(text)
+
+async def connect_user_voice(member: discord.Member, channel: discord.VoiceChannel):
+    '''유저 음성채널 이동'''
+    if member.voice is not None:
+        await member.move_to(channel)
+        print(f'[INFO] {member.name}을 {channel.name}으로 이동시킴')
