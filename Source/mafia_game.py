51162c5b37fa8517ffeea47ece35fe45b223eaac
MolecularPaper <jat05026@gmail.com>
2022-07-21 20:53:51 +0900

    Update

--- Source/mafia_game.py
+++ Source/mafia_game.py
@@ -1,13 +1,11 @@
-from distutils.command.config import config
-from click import command
-import discord, asyncio, json, utility
-from multiprocessing.dummy import Array
+import discord, asyncio, json
 from discord.ext import commands
+from Source import utility
 
 config_path = './Data/Mafia/mafia_game_config.json'
-text_path = './Data/Mafia/mafia_text.json'
-channel_path = './Data/Mafia/mafia_channel.json'
-trigger_path = './Data/Mafia/mafia_game_val.txt'
+text_path = './Data/Mafia/mafia_game_text.json'
+channel_path = './Data/Mafia/mafia_game_channel.json'
+trigger_path = './Data/Mafia/mafia_game_val.json'
 
 class Mafia(commands.Cog):
     def __init__(self, bot) -> None:
@@ -16,49 +14,35 @@ class Mafia(commands.Cog):
         self.systext = self.ReadJson(text_path) #게임 텍스트 설정
         self.channels = self.ReadJson(channel_path) #게임 채널 설정
         self.game_value = self.ReadJson(trigger_path) #게임 내부 트리거 설정
-
-        global prefix
-        prefix = self.config['prefix']
-
-        self.CreateChannel()
     
-    def ReadCommand(func, self, ctx, command, name=''):
-        def wrapper():
-            if command == name:
-                func(self, ctx)
-        return wrapper
-
-    @commands.command(name=prefix)
-    @ReadCommand(name='매치생성')
+    @commands.command(name=self.config['prefix'] + '_매치생성')
     async def Match(self, ctx):
+        await self.CreateChannel(ctx)
         await self.SendText(ctx, self.ConvertText(self.systext['match_making']['start']))
-
-    @commands.command(name=prefix)
-    @ReadCommand(name='매치중단')
+    
+    @commands.command(name=prefix + '_매치중단')
     async def MatchStop(self, ctx):
         await self.SendText(ctx, self.systext['match_making']['stop'])
         del self
-        
-    @commands.command(name=prefix)
-    @ReadCommand(name='게임시작')
+    
+    @commands.command(name=prefix + '_게임시작')
     async def GameStart(self, ctx):
         #유저수 검사후 시작 가능하면 시작
         await self.SendText(ctx, self.systext['match_making']['start'])
         return
-
-    commands.command(name=prefix)
-    @ReadCommand(name='게임 종료')
+    
+    @commands.command(name=prefix + '_게임종료')
     async def GameStop(self, ctx):
         await self.SendText(ctx, self.systext['match_making']['stop'])
         del self
 
-    async def ReadJson(self, path) -> dict:
+    def ReadJson(self, path) -> dict:
         with open(path, 'r', encoding='utf-8') as f:
             return json.load(f)
 
     async def CreateChannel(self, ctx):
-        for channel in self.channels.values():
-            self.channels[channel] = await utility.create_channel(ctx, channel['name'], channel['type'])
+        for key, value in self.channels.items():
+            self.channels[key]['channel'] = await utility.create_channel(ctx, self.bot, value['name'], value['type'], self.channels['category'])
 
     async def SendText(self, channel, text:str):
         await channel.send(text)
